7466648f88b59150e58d41efcdada23138ccce8a
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-03-31 20:41:37 -0700

    Better test feedback

--- test/xmltest.c
+++ test/xmltest.c
@@ -1,6 +1,9 @@
+#include <stdio.h>
 #include <libxml/xmlversion.h>
 
 int main() {
+	printf("Looking for LIBXML_VERSION: %d...", LIBXML_VERSION);
 	xmlCheckVersion(LIBXML_VERSION);
+	printf(" [OK]\n");
 	return 0;
 }
